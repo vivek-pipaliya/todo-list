8933a2d1f9327134a767e5e983b20ce97f0ebf12
vivek-pipaliya <vivekpialiya007@gmail.com>
2025-08-06 18:55:37 +0530

    store data in localstorage and add onload function

--- todo-list.js
+++ todo-list.js
@@ -1,32 +1,33 @@
 const addValue = document.getElementById("datainput"); // accessing input field
 const addButton = document.getElementById("addbutton"); // accessing add button
 const todo = document.getElementById("todolist"); // access the todolist id which stores the value/html
-const remove = document.querySelectorAll(".remove");
 
 let array = []; //creating an empty array
+let editIndex = null;
 
 addButton.addEventListener("click", () => {  // function for adding value
     const getValue = addValue.value;  //storing input value into variable
     // console.log(getValue)
     if (getValue) {
-        array.push(getValue) // add value to array
-        addValue.value = ""; // after adding value clearing the input field
-    }
-    repeate();
-})
 
+        array.push(getValue) // add value to array
+    }
+    addValue.value = ""; // after adding value clearing the input field
+    repeate();
+
+})
 
 let repeate = () => {
     todo.innerHTML = ""; // prevent from repeating value while adding new value
     for (let i = 0; i < array.length; i++) {
         let datainput = `<div class="form">
                             <label class="radio">${array[i]}
-                                <input class="input" type="checkbox" name="radio">
+                                <input class="input edit-button" type="checkbox" name="radio">
                                 <span class="checkmark"></span>
                             </label>
                             <div>
                                 <button class="button" class="delete" id="del">
-                                    <img src="./images/delete.svg" alt="delete">
+                                    <img class="delete-button" src="./images/delete.svg" alt="delete">
                                 </button>
                             </div>
                         </div>
@@ -35,14 +36,54 @@ let repeate = () => {
         document.getElementById("todolist").innerHTML += datainput; // adding value to container 
     }
 
-    const remove = document.querySelectorAll(".remove");
+    let stringValue = JSON.stringify(array);
+    localStorage.setItem("array", stringValue);
+    let valueBack = localStorage.getItem("array");
+    array = JSON.parse(valueBack);
+
+    const remove = document.querySelectorAll(".remove")
     remove.forEach((remove) => {
         remove.addEventListener("click", removeValue);
     })
 
+    const deleteButton = document.querySelectorAll(".delete-button")
+    deleteButton.forEach((deleteButton) => {
+        deleteButton.addEventListener("click", deleteValue);
+    })
+
+    const editButton = document.querySelectorAll('.edit-button');
+    editButton.forEach((editButton) => {
+        editButton.addEventListener("click", editValue);
+    })
 }
 
-let removeValue = () => {
-    array = [];
-    todo.innerHTML = "";
+const editValue = (event) => {
+    index = event.target.value;
+    addValue.value = array[index];
+    addButton.innerText = "EDIT"    
 }
+
+const removeValue = () => {
+    array = [];  // when clear button is clicked it will empty the array
+    todo.innerHTML = "";  // it will clear value from todo container
+    let stringValue = JSON.stringify(array);
+    localStorage.setItem("array", stringValue);
+    let valueBack = localStorage.getItem("array");
+    localStorage.removeItem(valueBack);
+}
+
+const deleteValue = (event) => {
+    const index = event.target.id;  // target index of array to delete 
+    array.splice(index, 1);  // delete value from array which is selected based on index
+    repeate();
+}
+
+window.onload = () => {
+    let valueBack = localStorage.getItem("array");
+    if (valueBack) {
+        array = JSON.parse(valueBack);
+        repeate();
+    }
+}
+
+
